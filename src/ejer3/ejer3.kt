c36cd59062fc4fbd5ae3127002e3199440e67d17
iara.zas <iarazasfernandez15@gmail.com>
2024-11-26 19:43:57 +0100

    dia 2

--- src/ejer3/ejer3.kt
+++ src/ejer3/ejer3.kt
@@ -4,46 +4,39 @@ fun main() {
     val expresiones = listOf(
         "{a + b [c] * (2x2)}}}}",
         "{ [ a * ( c + d ) - 5 ] }",
-        "{ a * ( c + d ) - 5 }",
-        "a^4 + ((4x4))",
+        "{ a * ( c + d ) ] - 5 }",
+        "{a^4 + (((ax4)}",
         "{ [ a * ( c + d ) + ( 2 - 3 ) ][ - 5 }",
-        "{{{{{{}}}}}}",
+        "{{{{{{(}}}}}}",
         "(a"
     )
-
     for (expresion in expresiones) {
-        val apertura = ArrayList<Char>()
-        val cierre = ArrayList<Char>()
+        val pila = ArrayList<Char>()
         var bien = true
-
         for (letra in expresion) {
             if (letra == '(' || letra == '{' || letra == '[') {
-                apertura.add(letra)
-            } else if (letra == ')' || letra == '}' || letra == ']') {
-                cierre.add(letra)
+                pila.add(letra)
             }
-            if (apertura.isEmpty() || !parejas(apertura.removeAt(apertura.size - 1), letra)) {
-                bien = false
-                break
-
+            else if (letra == ')' || letra == '}' || letra == ']') {
+                if (pila.isEmpty() || !parejas(pila.removeAt(pila.size - 1), letra)) {
+                    bien = false
+                    break
+                }
             }
-
-            if (apertura.isNotEmpty() || cierre.isNotEmpty()) {
-                bien = false
-            }
-
-
         }
+
+
+        if (pila.isNotEmpty()) {
+            bien = false
+        }
+
         println("$expresion $bien")
-
     }
-
 }
 
 fun parejas(abierto: Char, cerrado: Char): Boolean {
     return (abierto == '(' && cerrado == ')') ||
             (abierto == '{' && cerrado == '}') ||
             (abierto == '[' && cerrado == ']')
-
 }
 
